eddbe6f7600656491f2eddca66b8ef6bef9e5019
Sreethesh Pai <78226691+Sreethesh007@users.noreply.github.com>
2026-05-06 01:17:57 +0530

    model change

--- management/nlp_processor.py
+++ management/nlp_processor.py
@@ -60,7 +60,7 @@ User command: "{text}"
         """
         try:
             response = await self.client.aio.models.generate_content(
-                model='gemma-3-1b-it',
+                model='gemma-3-12b-it',
                 contents=prompt
             )
             # Remove any markdown formatting if present
